1545863d1421cd5929fa20f882886f34722f0a6f
Bonzz <ahmadfarisarhana@gmail.com>
2025-12-26 06:28:04 +0700

    Update Function SayMyName

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Bonnz123/Dert
+module github.com/Bonnz123/Dert/v2
 
 go 1.25.5
--- say.go
+++ say.go
@@ -1,11 +1,57 @@
 package dert
 
+import "strings"
+
+type TypeData struct {
+	Slice, Pointer, Byte, String, Reader string
+}
+
+
+type resultError struct {
+	Message string
+}
+
+type error interface {
+	Error() string
+}
+
+func (e *resultError) Error() string {
+	return e.Message
+}
+
+var TypeDataList = TypeData{
+	Pointer: "Pointer",
+	Byte: "Byte",
+	String: "String",
+	Reader: "Reader",
+}
+
 func SayHello() string {
 	return "Hello World"
 }
 
-func SayMyName(name string) string {
-	return "Hello " + name
+func SayMyName(name string, result TypeData) (any, error) {
+
+	if name == "" {
+		return nil, &resultError{
+			Message: "Name is empty",
+		}
+	}
+
+	if result.Pointer == TypeDataList.Pointer {
+		return &name, nil
+	} else if result.String == TypeDataList.String {
+		return name, nil
+	} else if result.Byte == TypeDataList.Byte {
+		return []byte(name), nil
+	} else if result.Reader == TypeDataList.Reader {
+		return strings.NewReader(name), nil
+	}
+
+	return nil, &resultError{
+		Message: "Error User Input",
+	}
+
 }
 
 
